56f3dd70172645b3091a09d8c3e90cabfb36be90
Mette <Mette@10.16.106.164>
2017-11-13 17:17:21 +0100

    testet hvordan GUI virker, oprettet et felt

--- src/Spil.java
+++ src/Spil.java
@@ -1,4 +1,21 @@
+import java.awt.Color;
+
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
+import gui_main.GUI;
 
 public class Spil {
+	
+	public static void main(String[] args) {
+	GUI gui = new GUI();
+	GUI_Field g= new GUI_Street();
+	g.setTitle("Test");
+	g.setBackGroundColor(Color.WHITE);
+	GUI_Field[] f = new GUI_Field[1];
+	f[0] = g;
+	new GUI(f);
+	gui.showMessage("test");
+	
+	}
 
 }
