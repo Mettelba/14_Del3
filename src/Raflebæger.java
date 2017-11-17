b212496c765c22e88237aa4627f1de6a87e909ff
Mette <Mette@Lenovo-PC.clients.wireless.dtu.dk>
2017-11-17 13:44:59 +0100

    Har fjernet terning2 fra Klassen Raflebæger

--- src/Raflebæger.java
+++ src/Raflebæger.java
@@ -1,66 +1,49 @@
 
 public class Raflebæger {
 
-	private int sum;
 	private int antalsider;
 	private Terning terning1;
+	private int terning1værdi;
 
 	
 
 	public void raflebæger (int antalsider) {	
 		
 		this.antalsider = antalsider;
-		terning1 = new Terning(this.antalsider);
-		terning2 = new Terning(this.antalsider);
-		
-		
+		terning1 = new Terning(this.antalsider);	
 	}
 
 
 	public int hentAntalsider() {
 		return antalsider;
-
 	}
 
 
+	public void ryst() {
+		this.terning1værdi = 0;
+		
+		terning1.kast();		
+		
+		this.terning1værdi = this.terning1.hentøjne();	
+	}
+	
+	
+	public int hentTerning1værdi(){
+		terning1værdi = terning1.hentøjne();
+		return terning1værdi;
+ 	}
+	
+	
 	public void sætAntalsider(int antalsider) {
 		this.antalsider = antalsider;
 
 	}
-
-
-	public int hentSum() {
-		sum = terning1.hentøjne() + terning2.hentøjne();
-		return sum;
+	
+	
+ 	public void sætTerning1værdi(int terning1værdi) {
+ 		this.terning1værdi = terning1værdi;
+ 	}
 		
-
-	}
-
-
-	public void sætSum(int sum) {
-		this.sum = sum;
-
-	}
-
-	public void ryst() {
-		this.sum = 0;
-		
-		terning1.kast();		
-		terning2.kast();
-		
-		sum = this.terning1.hentøjne() + this.terning2.hentøjne();
-		
-		
-		
-	}
-	//en metode der henter værdien fra terning1 
-		public int hentTerning1værdi () {
-			return terning1.hentøjne();
-			}
-		
-		//en metode der henter værdien fra terning1 
-		public int hentTerning2værdi () {
-			return terning2.hentøjne();
-		}
+	
 	
 }
