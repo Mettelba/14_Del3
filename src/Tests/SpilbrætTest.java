452a945a73bb4869e1a4d0979e5ccb5665c93571
Tamou <Tamou@DESKTOP-V6QN5QC.clients.wireless.dtu.dk>
2017-11-21 12:31:07 +0100

    Test af Spilbræt

---
+++ src/Tests/SpilbrætTest.java
@@ -0,0 +1,67 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import Felter.Felt;
+
+
+public class SpilbrætTest {
+	private Spiller[] spiller = new Spiller[1];
+
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+
+
+
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testHentSpilGui() {
+		
+			}
+
+	@Test
+	public void testHentSpilFelter() {
+		
+	
+		
+	}
+
+	@Test
+	public void testHentType() {
+		
+		spiller[0] = new Spiller();
+		Spilbræt spillebræt = new Spilbræt(spiller);
+		Felt[] felter;
+		felter = spillebræt.hentSpilFelter();
+		
+		int expected = 2;
+		int actual = felter[15].hentFeltType();
+		assertEquals("der er noget galt med felttypen",expected, actual);
+		// Testet med felter ( 5, 12, 15, 21)
+
+	}
+}
+
