0f77354fbf1c1ad0e99e8b00cea765df41627efd
Mette <Mette@10.16.108.229>
2017-11-24 10:35:10 +0100

    rettet i raflebægertest til 1 terning

--- src/Tests/RaflebægerTest.java
+++ src/Tests/RaflebægerTest.java
@@ -72,31 +72,39 @@ public class RaflebægerTest {
 		int actual = raflebæger.hentTerning1værdi();
 		assertEquals("metoden hentTerning virker ikke", expected, actual);
 	}
-
-	@Test
-	public void testHentTerning2værdi() {
-		raflebæger.sætTerning2værdi(8);
-		int expected = 8;
-		int actual = raflebæger.hentTerning2værdi();
-		assertEquals("metoden hentTerning virker ikke", expected, actual);
-	}
-
-	@Test
-	public void testHentSum() {
-		raflebæger.sætSum(3);
-		int expected = 3;
-		int actual = raflebæger.hentSum();
-		assertEquals("hentSum virker ikke", expected, actual);
-	}
 	
 	@Test
-	public void testSætSum() {
-		raflebæger.sætSum(6);
-		int expected = 6;
-		int actual = raflebæger.hentSum();
-		assertEquals("sætSum virker ikke", expected, actual);
-		
+	public void testsætTerning1værdi() {
+		raflebæger.sætTerning1værdi(5);
+		int expected = 5;
+		int actual = raflebæger.hentTerning1værdi();
+		assertEquals("metoden sætTerning1værdi virker ikke", expected, actual);
 	}
+
+//	@Test
+//	public void testHentTerning2værdi() {
+//		raflebæger.sætTerning2værdi(8);
+//		int expected = 8;
+//		int actual = raflebæger.hentTerning2værdi();
+//		assertEquals("metoden hentTerning virker ikke", expected, actual);
+//	}
+
+//	@Test
+//	public void testHentSum() {
+//		raflebæger.sætSum(3);
+//		int expected = 3;
+//		int actual = raflebæger.hentSum();
+//		assertEquals("hentSum virker ikke", expected, actual);
+//	}
+	
+//	@Test
+//	public void testSætSum() {
+//		raflebæger.sætSum(6);
+//		int expected = 6;
+//		int actual = raflebæger.hentSum();
+//		assertEquals("sætSum virker ikke", expected, actual);
+//		
+//	}
 	
 	@Test
 	public void testRyst() {
@@ -111,26 +119,26 @@ public class RaflebægerTest {
 			System.out.println("terning1 " + x+"ere = " + statistik[x]);
 		}	
 		
-		int[] statistik1 = new int[4];
-		for(int n=1; n<600000; n=n+1) {
-			raflebæger.ryst();
-			int sum1 = raflebæger.hentTerning2værdi();
-			statistik1[sum1] = statistik1[sum1]+1;
-		}
-		for(int x=1; x<=3; x=x+1) {
-			System.out.println("terning2 " + x+"ere = " + statistik1[x]);
-		}	
-		
-		int[] statistik2 = new int[7]; 
-		for(int n=1; n<600000; n=n+1) {
-			raflebæger.ryst();
-			int sum2 = raflebæger.hentSum();
-
-			statistik2[sum2] = statistik2[sum2]+1;
-		}
-		for(int x=2; x<=6; x=x+1) {
-			System.out.println("Sum af terning1 og terning2 " + x+"ere = " + statistik2[x]);
-		}
+//		int[] statistik1 = new int[4];
+//		for(int n=1; n<600000; n=n+1) {
+//			raflebæger.ryst();
+//			int sum1 = raflebæger.hentTerning2værdi();
+//			statistik1[sum1] = statistik1[sum1]+1;
+//		}
+//		for(int x=1; x<=3; x=x+1) {
+//			System.out.println("terning2 " + x+"ere = " + statistik1[x]);
+//		}	
+//		
+//		int[] statistik2 = new int[7]; 
+//		for(int n=1; n<600000; n=n+1) {
+//			raflebæger.ryst();
+//			int sum2 = raflebæger.hentSum();
+//
+//			statistik2[sum2] = statistik2[sum2]+1;
+//		}
+//		for(int x=2; x<=6; x=x+1) {
+//			System.out.println("Sum af terning1 og terning2 " + x+"ere = " + statistik2[x]);
+//		}
 	}
  
 }
