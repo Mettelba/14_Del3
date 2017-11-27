f6ade2512e0fbf22a57e6f47b6f237e272cf9e75
Mettelba <s172840@student.dtu.dk>
2017-11-27 23:19:42 +0100

    Har fordelt ansvaret fra spil til en ny klasse: Startspilcontroller.

--- src/Game/Spil.java
+++ src/Game/Spil.java
@@ -18,32 +18,10 @@ public class Spil {
 	}
 	
 	public void initialiserSpil() {
-		String indtastetnavn;
+	    StartSpilController startSpilController = new StartSpilController();
+	    spiller = startSpilController.initialiserSpil();
+	    
 
-		//Indtast antal spillere og deres navne
-		System.out.print("Indtast antal spillere fra 1 til 4: ");
-		antalspillere = Integer.parseInt(sc.nextLine());
-		System.out.println();
-
-		if (antalspillere < 1 || antalspillere > 4) {
-			return;
-		}
-
-		//opret antalspillere + 1. Spiller0 er spillet.
-		spiller = new Spiller[antalspillere+1];
-
-		//overfør spillernavne til spillerobjekter
-		for (int i = 0; i<=antalspillere; i++) {
-			spiller[i] = new Spiller();
-		}
-
-		// sætter spillernavne i spillerobjekterne
-		for (int x=1 ; x<= antalspillere;x++) {
-			System.out.print("Indtast navn på spiller "+ x +" :");
-			indtastetnavn = sc.nextLine();
-			System.out.println();
-			spiller[x].sætNavn(indtastetnavn);
-		}
 		spilkontrol = new SpilController(spiller);
 		spilkontrol.skiftSpiller(antalspillere);
 	}
---
+++ src/Game/StartSpilController.java
@@ -0,0 +1,40 @@
+package Game;
+
+import java.util.Scanner;
+
+public class StartSpilController {
+
+	
+	public Spiller[] initialiserSpil() {
+		String indtastetnavn;
+
+		//Indtast antal spillere og deres navne
+		Scanner sc = new Scanner(System.in);
+		
+		int antalspillere = 0;
+		do {
+		System.out.print("Indtast antal spillere fra 1 til 4: ");
+		antalspillere = Integer.parseInt(sc.nextLine());
+		System.out.println();
+
+		} while (antalspillere < 1 || antalspillere > 4) ;
+			
+
+		//opret antalspillere + 1. Spiller0 er spillet.
+		Spiller[] spiller = new Spiller[antalspillere+1];
+
+		//overfør spillernavne til spillerobjekter
+		for (int i = 0; i<=antalspillere; i++) {
+			spiller[i] = new Spiller();
+		}
+
+		// sætter spillernavne i spillerobjekterne
+		for (int x=1 ; x<= antalspillere;x++) {
+			System.out.print("Indtast navn på spiller "+ x +" :");
+			indtastetnavn = sc.nextLine();
+			System.out.println();
+			spiller[x].sætNavn(indtastetnavn);
+		}
+		 return spiller; //giver referencen tilbage 
+	}
+}
